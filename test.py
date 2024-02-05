78a462773f88190b85d5f546ca34e94acc81e5bf
naveendas <naveendas1997@gmail.com>
2024-02-06 01:30:17 +0530

    Update test.py

--- test.py
+++ test.py
@@ -8,9 +8,9 @@ client = TestClient(app)
 def test_word_frequency():
     response = client.get("/wordFrequency?topic=python&top_n=5")
     assert response.status_code == 200
-    assert "Python programming language" in response.json()
+    assert "Python" in response.json()
 
 def test_search_history():
     response = client.get("/searchHistory")
     assert response.status_code == 200
-    assert "Search History" in response.json()
+    assert "topic" in response.json()
